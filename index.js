a354b09048d834c61cce9fa4fbee90eee3af502c
Manupriyan B S <108547134+manu-git-b-s@users.noreply.github.com>
2024-03-21 18:39:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./database/dbConfig.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send(`<h1 style=text-align:center>Welcome to URL Shortener Backend<h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Connected to the port`, port);
+});
